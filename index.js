29584b5d3c94ef4dc755b6f9e14e1d95d77bdb92
Collins Chirchir <collinskchirchir@gmail.com>
2022-12-14 12:58:55 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
-// Add your code here
+function submitData(username, mail)
+{
+  const credentials = {
+    name: username,
+    email: mail};
+  console.log(credentials)
+  const configurationObject = {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      Accept: 'application/json'
+    },
+    body: JSON.stringify(credentials)
+  }
+  console.log(configurationObject)
+  return fetch(`http://localhost:3000/users`, configurationObject)
+    .then(response => response.json())
+    .then(data => renderData(data))
+    .catch(error => renderError(error))
+  
+}
+
+function renderData(data){
+  const body = document.querySelector('body');
+  const responseId = document.createElement('h1');
+  responseId.textContent = data.id;
+  body.appendChild(responseId);
+}
+function renderError(error)
+{
+  const errorMessage = document.createElement('p');
+  errorMessage.textContent = error.message;
+  const body = document.querySelector('body')
+  body.append(errorMessage);
+}
+
+document.addEventListener('DOMContentLoaded',()=> {
+  submitData("kevin", "kevin@gmail.com")
+})
